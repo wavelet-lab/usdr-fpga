948c718b2a88dc95d7abea7109a932fb2b77b395
Sergey Kostanbaev <sergey.kostanbaev@gmail.com>
2026-03-27 04:25:29 +0400

    IQ CORR fix

--- lib/dsp/iq_corr.v
+++ lib/dsp/iq_corr.v
@@ -1,17 +1,19 @@
 module iq_corr #(
-    parameter WIDTH    = 12
+    parameter WIDTH     = 12,
+    parameter CFG_WIDTH = 16,
+    parameter OUT_WIDTH = 16
 ) (
     input clk,
     input rst,
 
-    input [WIDTH * 2 - 1:0]      cfg_amp,
-    input [WIDTH * 2 - 1:0]      cfg_tan,
+    input [CFG_WIDTH * 2 - 1:0]  cfg_amp,
+    input [CFG_WIDTH * 2 - 1:0]  cfg_tan,
 
     input [WIDTH * 2 - 1:0]      in_data,
     input                        in_valid,
 
-    output reg [WIDTH * 2 - 1:0] out_data,
-    output reg                   out_valid
+    output     [OUT_WIDTH * 2 - 1:0] out_data,
+    output reg                       out_valid
 
 );
 
@@ -26,12 +28,18 @@ module iq_corr #(
 //  /                 \
 // ------ | ---------| * [] + |---
 
-localparam EXT_WIDTH = 2 * WIDTH - 1; // Sign-extended double
+localparam EXT_WIDTH = (CFG_WIDTH + WIDTH) - 1; // Sign-extended double
 
 
 reg [WIDTH * 2 - 1:0]     data_s0_direct;
-reg [EXT_WIDTH * 2 - 1:0] data_s0_mtan;
+
+reg [EXT_WIDTH - 1:0]     data_s0_mtan_i;
+reg [EXT_WIDTH - 1:0]     data_s0_mtan_q;
+
 reg                       data_s0_valid;
+
+reg [EXT_WIDTH - 1:0]     data_s1_corr_i;
+reg [EXT_WIDTH - 1:0]     data_s1_corr_q;
 
 always @(posedge clk) begin
     if (rst) begin
@@ -43,17 +51,22 @@ always @(posedge clk) begin
     end
 end
 
-
 always @(posedge clk) begin
     if (in_valid) begin
-        data_s0_mtan <= in_data;
+        data_s0_direct <= in_data;
+        data_s0_mtan_i <= $signed(in_data[WIDTH - 1:0])         * $signed(cfg_tan[CFG_WIDTH - 1:0]);
+        data_s0_mtan_q <= $signed(in_data[2 * WIDTH - 1:WIDTH]) * $signed(cfg_tan[2 * CFG_WIDTH - 1:CFG_WIDTH]);
     end
 
     if (data_s0_valid) begin
-        out_valid <= data_s0_mtan;
+        data_s1_corr_i <= $signed(data_s0_direct[WIDTH - 1:0])         * $signed(cfg_amp[CFG_WIDTH - 1:0])             + data_s0_mtan_q;
+        data_s1_corr_q <= $signed(data_s0_direct[2 * WIDTH - 1:WIDTH]) * $signed(cfg_amp[2 * CFG_WIDTH - 1:CFG_WIDTH]) + data_s0_mtan_i;
     end
 end
 
-
+assign out_data = {
+    data_s1_corr_q[EXT_WIDTH - 1:EXT_WIDTH - OUT_WIDTH],
+    data_s1_corr_i[EXT_WIDTH - 1:EXT_WIDTH - OUT_WIDTH]
+};
 
 endmodule
